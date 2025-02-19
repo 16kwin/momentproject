f55408d3b1b9d9fe425dbfb01689fe69f5593512
16kwin <DKM_PSVU@mail.ru>
2025-02-19 12:11:44 +0500

    Initial commit

---
+++ src/main/java/com/project/momentproject/MomentprojectApplication.java
@@ -0,0 +1,15 @@
+package com.project.momentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MomentprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentprojectApplication.class, args);
+    }
+
+}
